65a8b10c732d1d35019e6165d484a3bdc7a788d1
Tim Schmucki <timschmucki@bluewin.ch>
2020-10-27 10:59:24 +0100

    Adapted the structure of the Shiny App to our project goal - now, the Shiny App manages to visualize stock price developments by retrieving data from Yahoo!Finance.

--- TradingGeeks.R
+++ TradingGeeks.R
@@ -7,62 +7,96 @@
 
 #get relevant libraries
 library(shiny) # to build interactive web apps straight from R
+library(quantmod) # for quantitative financial modelling
+library(PerformanceAnalytics) # economic tool for performance analysis 
 
-### First, define basic structure of R Shiny according to shiny.rstudio.com
+# --------------------------------------------------------------
+# USER INTERFACE SHINY APP -------------------------------------
+# --------------------------------------------------------------
 
-# Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Hello Shiny!"),
+  # Enter the title of the program
+  titlePanel("Trading Strategy Backtesting"),
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
-  )
-)
+  ##### Define the layout of the shiny app ####
+  sidebarLayout(position = "left",
+                
+                ### In the sidebar panel, inputs can be specified by the user ###
+                sidebarPanel(
+                  
+                  ## General Information about the program
+                  h3(strong("Instructions")),
+                  helpText("Placeholder for an adequate program description"),
+                  br(), # ads a break to increase clarity
+                  
+                  ## Input field to define the stock
+                  h4(strong("1) Choose a Financial Security")),
+                  helpText("Please enter a ticker.
+               For instance, use 'AAPL' or 'MSFT' for Apple or Microsoft, respectively.
+               For further examples, please visit 'www.finance.yahoo.com'."),
+                  textInput(inputId = "ticker",
+                            label = "Ticker (in capital letters):",
+                            value = "AAPL"),
+                  br(), # ads a break to increase clarity
+                  
+                  ## Input fields to define date range
+                  h4(strong("2) Specify Date Range")),
+                  dateRangeInput(inputId = "dates", 
+                                 label= "Date range:",
+                                 start = "2018-01-01",
+                                 end =   as.character(Sys.Date())), # the most current date will be the default value
+                  br(), # ads a break to increase clarity
+                  
+                  ## Dropdown to select chart type
+                  h4(strong("3) Select Chart Type")),
+                  selectInput(inputId = "chart_type",
+                              label = "Chart Type:",
+                              choices=list("line", "candlesticks", "matchsticks", "bars")),
+                  br(), # ads a break to increase clarity
+                  
+                  submitButton("Submit"),
+                  br(), # ads a break to increase clarity
+                  
+                  
+                ), # sidebar panel closing bracket
+                
+                ### In the main panel, the charts will be displayed ###
+                mainPanel(
+                  
+                  plotOutput("ticker"), # visualizes the performance of the stock
+                  br(), # ads a break to increase clarity
+                  
+                ) # main panel closing bracket
+  ) # sidebar layout closing bracket
+) # fluid page closing bracket
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
-  
-}
+# --------------------------------------------------------------
+# SERVER FUNCTION SHINY APP ------------------------------------
+# --------------------------------------------------------------
 
-shinyApp(ui, server)
+server <- function(input, output){
+  
+  output$ticker <- renderPlot({ # Server Function which visualizes the stock price development
+    
+    # First, the stock data is retrieved from Yahoo based on the user input
+    getSymbols(as.character(input$ticker), # input$ticker gets the symbol of the stock which was entered by the user (e.g. AAPL)
+               src="yahoo", # stock data is retrieved from Yahoo
+               from = input$dates[1], # gets first input date (start date)
+               to = input$dates[2]) # gets second input date (end date)
+    
+    chartSeries(eval(parse(text=input$ticker)), 
+                name=paste("Chart to analyze the stock:", input$ticker), 
+                type=input$chart_type, 
+                up.col='black', dn.col='red', theme="white")
+    
+  }) # closing brackets renderPlot output$ticker
+  
+  
+} # closing bracket server function
+
+# --------------------------------------------------------------
+# COMBINATION OF UI & SERVER -----------------------------------
+# --------------------------------------------------------------
+
+shinyApp(ui = ui, server = server)
