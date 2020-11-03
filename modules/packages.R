462ce220cd0ca81f778945e5aae5c669353b404f
Jan Scheiddegger <jan_s@Jans-MacBook-Pro.local>
2020-11-03 13:29:08 +0100

    Loads the required library packages

---
+++ modules/packages.R
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------
+# Load Libraries ------------------------------------------------
+# --------------------------------------------------------------
+
+library(shiny) # to build interactive web apps straight from R
+library(quantmod) # for quantitative financial modelling
+library(PerformanceAnalytics) # economic tool for performance analysis 
