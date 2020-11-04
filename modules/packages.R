1617ba74f4b0512aef0854f75e5437df24467920
Tim Schmucki <timschmucki@bluewin.ch>
2020-11-04 19:53:06 +0100

    Included library (TTR)

--- modules/packages.R
+++ modules/packages.R
@@ -4,4 +4,5 @@
 
 library(shiny) # to build interactive web apps straight from R
 library(quantmod) # for quantitative financial modelling
-library(PerformanceAnalytics) # economic tool for performance analysis 
+library(PerformanceAnalytics) # economic tool for performance analysis
+library(TTR) # to construct and apply technical trading rules 
