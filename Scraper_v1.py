f3efed4850eb4b11cc5bd020406d9291f586d4d6
stefan <sdiener7@gmail.com>
2020-11-10 11:07:21 +0100

    Scraper_v1.py fills search mask

--- Scraper_v1.py
+++ Scraper_v1.py
@@ -1,7 +1,8 @@
 import getopt
 import sys
 import os
-
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 def main(argumentlist):
     print("Scraping started")
@@ -32,7 +33,44 @@ def main(argumentlist):
 
     dirname = os.path.dirname(__file__)  # dirname = "C:/Users/sdien/PycharmProjects/MyFCurve/Data/News"
 
+    url = "https://zeitungsarchiv.nzz.ch/#archive"
 
+    # Operating in headless mode
+    opts = Options()
+    opts.headless = True
+
+    # Start Browser and search for articles in German containing words: Inflation, Arbeitsmarkt, Konjunktur
+    browser = webdriver.Firefox(options=opts, executable_path=os.path.join(dirname, "geckodriver.exe"))
+    browser.get(url)
+
+    time.sleep(5)
+
+    for date in daterange:
+        print("Scraping articles from {}".format(date))
+        datestr = date.strftime("%d.%m.%Y")
+
+        key = browser.find_element_by_class_name("fup-archive-query-input")
+        key.clear()
+        key.send_keys(searchkey)
+
+        time.sleep(1)
+
+        beg = browser.find_element_by_class_name("fup-s-date-start")
+        beg.clear()
+        beg.send_keys(datestr)
+
+        time.sleep(1)
+
+        end = browser.find_element_by_class_name("fup-s-date-end")
+        end.clear()
+        end.send_keys(datestr)
+
+        time.sleep(1)
+
+        such = browser.find_element_by_class_name("fup-s-exec-search")
+        such.click()
+
+        time.sleep(10)
 
 if __name__ == "__main__":
     main(sys.argv)
