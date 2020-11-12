62ead01894d577c660e78e1716c7aa83276321d6
stefan <sdiener7@gmail.com>
2020-11-12 10:09:41 +0100

    Improved Scraper_v1.py structure

--- Scraper_v1.py
+++ Scraper_v1.py
@@ -1,153 +1,192 @@
+# %% Setup
 import getopt
 import sys
 import time
 import math
-import string
 import os
 
+import numpy
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# %% Functions
+def getCmdInput(argv):
+    """ Parse the command line arguments. """
+    argv = argv[1:]  # remove path (arg[0])
 
-def main(argumentlist):
-    print("Scraping started")
-    nrarguments = (len(argumentlist) - 1) / 2
-    argumentlist = argumentlist[1:]
+    # Get options and arguments from command line input
     try:
-        short_options = "hk:s:e:"
-        long_options = ["help", "searchey=", "startdate=", "enddate="]
-        arguments, values = getopt.getopt(argumentlist, short_options, long_options)
-    except getopt.GetoptError:
-        print('tagi_args.py -k <searchkey> -s <startdate %d.%m-%Y> -e <enddate %d.%m-%Y>')
+        opts, args = getopt.getopt(argv, "q:s:e:")
+    except getopt.GetoptError as err:
+        print("Command line error: {}".format(err))
         sys.exit(2)
 
-    for current_argument, current_value in arguments:
-        if current_argument in ("-k", "--searchkey"):
-            searchkey = current_value
-        elif current_argument in ("-h", "--help") or nrarguments != 3:
-            print('tagi_args.py -k <searchkey> -s <startdate %Y-%m-%d> -e <enddate %Y-%m-%d>')
-            sys.exit()
-        elif current_argument in ("-s", "--startdate"):
-            strt = current_value
-        elif current_argument in ("-e", "--enddate"):
-            end = current_value
+    # Define variables
+    for opt, arg in opts:
+        if opt in ("-q"):
+            searchquery = arg
+        elif opt in ("-s"):
+            start_date = arg
+        elif opt in ("-e"):
+            end_date = arg
 
-    if len(searchkey.split()) != 2:
-        print("Two searchkeys needed. Only got one: '{}'".format(searchkey))
-        sys.exit()
-
-    dirname = os.path.dirname(__file__)  # dirname = "C:/Users/sdien/PycharmProjects/MyFCurve/Data/News"
+    return searchquery, start_date, end_date
 
 
-    if "schweiz" in searchkey.split()[1]:
-        print(searchkey.split()[1])
-        path = os.path.join(dirname, 'NZZ/dom').replace("\\", "/")
+def selectSaveDir(searchquery):
+    """ Select saving directory based on query. """
+
+    if "schweiz" in searchquery.split():
+        path = 'Data/News/NZZ/dom'
     else:
-        print(searchkey.split()[1])
-        path = os.path.join(dirname, 'NZZ/for').replace("\\", "/")
+        path = 'Data/News/NZZ/for'
 
-    url = "https://zeitungsarchiv.nzz.ch/#archive"
+    return path
 
+
+def initBrowser(url):
+    """ Initialize selenium browser. """
     # Operating in headless mode
     opts = Options()
-    opts.headless = True
+    opts.headless = False
 
     # Start Browser and search for articles in German containing words: Inflation, Arbeitsmarkt, Konjunktur
-    browser = webdriver.Firefox(options=opts, executable_path=os.path.join(dirname, "geckodriver.exe"))
+    browser = webdriver.Firefox(options=opts, executable_path=os.path.dirname(__file__) + "/geckodriver.exe")
     browser.get(url)
 
     time.sleep(5)
 
-    daterange = pd.date_range(start=strt, end=end)
+    return browser
+
+
+def executeSearch(browser, searchquery, date):
+    """ Populates NZZ archive mask and executes search. """
+    # Convert date to correct format
+    datestr = date.strftime("%d.%m.%Y")
+
+    # Populate search mask
+    query = browser.find_element_by_class_name("fup-archive-query-input")
+    query.clear()
+    query.send_keys(searchquery)
+
+    time.sleep(1)
+
+    strt = browser.find_element_by_class_name("fup-s-date-start")
+    strt.clear()
+    strt.send_keys(datestr)
+
+    time.sleep(1)
+
+    end = browser.find_element_by_class_name("fup-s-date-end")
+    end.clear()
+    end.send_keys(datestr)
+
+    time.sleep(1)
+
+    # Execute search
+    such = browser.find_element_by_class_name("fup-s-exec-search")
+    such.click()
+
+    time.sleep(10)
+
+
+def scrollToBottom(browser):
+    """ Scolls to bottom of page to load all articles. """
+
+    # Calculate number of hits for search query
+    nr_hits = browser.find_element_by_class_name("fup-archive-result-hits").text
+    nr_hits = int(nr_hits.split()[0])
+    # try:
+    #     pass
+    # except Exception as err:
+    #     print("Error reading nr of hits: ", err)
+        # browser.refresh()
+
+        # time.sleep(10)
+
+        # nr_hits = browser.find_element_by_class_name("fup-archive-result-hits").text
+        # nr_hits = int(nr_hits.split()[0])
+
+    # Calculate number of scrolls
+    nr_scrolls = math.ceil(nr_hits / 50)
+
+    # Perform the scrolls
+    for i in range(nr_scrolls):
+        try:
+            actions = ActionChains(browser)
+            element = browser.find_elements_by_class_name("fup-common-scroll")[1]
+            length = element.size["height"]
+            browser.execute_script("arguments[0].scrollIntoView();", element)
+            actions.move_to_element_with_offset(element, 4, length - 5)
+            actions.click()
+            actions.perform()
+            WebDriverWait(browser, 5).until_not(EC.invisibility_of_element_located((By.CLASS_NAME, "fup-archive"
+                                                                                                   "-result-loader")))
+            time.sleep(2)
+
+            print("Scrolling worked. Hits: {}, Clicks: {}".format(nr_hits, nr_scrolls))
+        except Exception as err:
+            print("Scrolling failed. Hits: {}, Clicks: {}".format(nr_hits, nr_scrolls))
+            print("Err msg: ", err)
+            break
+
+def extractText(browser, date):
+    """ Extracts text and title of newspaper archives. """
+    parent = browser.find_elements_by_class_name("fup-archive-result-item")[1:]
 
     list_date = []
     list_text = []
     list_title = []
 
-    translator = str.maketrans('', '', string.punctuation)
+    for div in parent:
+        title = div.find_element_by_class_name("fup-archive-result-item-article-title").get_attribute('innerHTML')
+        text = div.find_element_by_class_name("fup-archive-result-item-article-text").get_attribute('innerHTML')
+        list_date.append(date.strftime("%d.%m.%Y"))
+        list_text.append(text)
+        list_title.append(title)
 
+    df = pd.DataFrame({'date': list_date, 'title': list_title, 'text': list_text})
+
+    return df
+
+
+# %% Main
+
+def main():
+    # Read command line input
+    searchquery, start_date, end_date = getCmdInput(sys.argv)
+
+    # Select saving directory based on query
+    savedir = selectSaveDir(searchquery)  # path for saving data
+
+    # Start selenium webdriver
+    url = "https://zeitungsarchiv.nzz.ch/#archive"
+    browser = initBrowser(url)
+    print("Scraping NZZ archive...")
+
+    # Loop through each day and scrape articles
+    daterange = pd.date_range(start=start_date, end=end_date)
     for date in daterange:
-        print("Scraping articles from {}".format(date))
-        datestr = date.strftime("%d.%m.%Y")
+        print(date)
+        executeSearch(browser, searchquery, date)
+        scrollToBottom(browser)
 
-        key = browser.find_element_by_class_name("fup-archive-query-input")
-        key.clear()
-        key.send_keys(searchkey)
+        df = extractText(browser, date)
 
-        beg = browser.find_element_by_class_name("fup-s-date-start")
-        beg.clear()
-        beg.send_keys(datestr)
+        df.to_excel(savedir + "/" + searchquery + "_" + str(date.strftime("%Y%m%d")) + ".xlsx", index=False)
 
-        end = browser.find_element_by_class_name("fup-s-date-end")
-        end.clear()
-        end.send_keys(datestr)
-
-        such = browser.find_element_by_class_name("fup-s-exec-search")
-        such.click()
-
-        time.sleep(10)
-
-        nr = browser.find_element_by_class_name("fup-archive-result-hits").text
-        print("nr = {}".format(nr))
-            try:
-                nr = int(nr.split()[0])
-            except Exception as e:
-                print(e)
-                such.click()
-
-                time.sleep(10)
-
-                nr = browser.find_element_by_class_name("fup-archive-result-hits").text
-                nr = int(nr.split()[0])
-
-        nr_clicks = math.ceil(nr / 50)
-
-        for ii in range(nr_clicks):
-            try:
-                actions = ActionChains(browser)
-                element = browser.find_elements_by_class_name("fup-common-scroll")[1]
-                length = element.size["height"]
-                browser.execute_script("arguments[0].scrollIntoView();", element);
-                actions.move_to_element_with_offset(element, 4, length - 5)
-                actions.click()
-                actions.perform()
-                WebDriverWait(browser, 5).until_not(
-                    EC.invisibility_of_element_located((By.CLASS_NAME, "fup-archive-result-loader")))
-                time.sleep(2)
-            except Exception as e:
-                print(e)
-                break
-
-        parent = browser.find_elements_by_class_name("fup-archive-result-item")[1:]
-
-        for div in parent:
-            title = div.find_element_by_class_name("fup-archive-result-item-article-title").get_attribute('innerHTML')
-            text = div.find_element_by_class_name("fup-archive-result-item-article-text").get_attribute('innerHTML')
-            list_date.append(date.strftime("%d.%m.%Y"))
-            list_text.append(text)
-            list_title.append(title)
-
-        fname = searchkey.translate(translator)
-
-        df = pd.DataFrame({'date': list_date, 'title': list_title, 'text': list_text})
-        df.to_excel(path + "/" + fname + "_" + str(date.strftime("%Y%m%d")) + ".xlsx", index=False)
-        list_date = []
-        list_text = []
-        list_title = []
         time.sleep(2)
 
     browser.close()
     browser.quit()
 
 
-if __name__ == "__main__":
-    main(sys.argv)
+# %% Run
 
-    #argumentlist = [sys.argv[0], "-k", "rezession schweiz", "-s", "2020-10-01", "-e", "2020-10-11"]
-    #main(argumentlist)
+if __name__ == "__main__":
+    main()
