85570d25d4a318629a8cca133024c91c84f0e95c
Jan Scheidegger <55899065+jan4j@users.noreply.github.com>
2020-11-24 10:27:28 +0100

    New required packages

--- modules/packages.R
+++ modules/packages.R
@@ -6,3 +6,5 @@ library(shiny) # to build interactive web apps straight from R
 library(quantmod) # for quantitative financial modelling
 library(PerformanceAnalytics) # economic tool for performance analysis
 library(TTR) # to construct and apply technical trading rules 
+library(shinycssloaders) #for loader of plots 
+library(shinyjs) #for hidden button of twitter
